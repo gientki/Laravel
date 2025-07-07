7429c6f105e7e509ef1489cb7a7ab121a7cdfe2a
gientki <74157161+gientki@users.noreply.github.com>
2025-07-07 15:38:32 +0200

    Delete Larawer-petstore directory

--- Larawer-petstore/resources/views/errors/404.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Błąd 404</h1>
-    <p>Strona nie została znaleziona.</p>
-    <a href="{{ route('pets.index') }}" class="btn btn-primary">Powrót do strony głównej</a>
-@endsection
--- Larawer-petstore/resources/views/errors/419.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Błąd 419</h1>
-    <p>Sesja wygasła. Odśwież stronę i spróbuj ponownie.</p>
-    <a href="{{ route('pets.index') }}" class="btn btn-primary">Odśwież</a>
-@endsection
